fce0928fd725e5cada51ed849b0a2bd206b95130
sjh50200 <sjh2273@naver.com>
2022-03-08 17:48:31 +0900

    feat: router 설정 완료

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../pages";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default index;
